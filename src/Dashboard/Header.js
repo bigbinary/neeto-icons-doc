095c04048d423dd7e0deb96bedac565b7465db79
Vinay Chandran <vinaychandranvs@gmail.com>
2021-06-30 19:36:52 +0530

    Implement toast

--- src/Dashboard/Header.js
+++ src/Dashboard/Header.js
@@ -3,12 +3,13 @@ import React from "react";
 export default function Header({ searchTerm, setSearchTerm }) {
 	return (
 		<>
-			{/* <nav className="w-full px-8 py-4 bg-gray-50">
-      <h1 className="text-2xl font-bold">neetoIcons</h1>
-    </nav> */}
-
 			<header className="flex items-center justify-between px-8 py-4">
-				<h1 className="text-2xl font-bold">neetoIcons</h1>
+				<div>
+					<h1 className="text-2xl font-bold">neetoIcons</h1>
+					<div className="text-sm text-gray-500">
+						Click on an icon to copy the name.
+					</div>
+				</div>
 				<input
 					placeholder="Search for an icon"
 					value={searchTerm}
--- src/Dashboard/IconList/IconItem.js
+++ src/Dashboard/IconList/IconItem.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React from "react";
 import * as Icons from "@bigbinary/neeto-icons";
+import copy from "copy-to-clipboard";
+import toast from "react-hot-toast";
 
 export default function IconItem({ name }) {
-  const Icon = Icons[name];
-  return (
-    <div className="flex flex-col items-center justify-center col-span-1 p-8 transition-all transform rounded cursor-pointer bg-gray-50 hover:bg-gray-100">
-      <Icon size={24} color="black"/>
-      <div className="mt-2 text-xs text-gray-700">{name}</div>
-    </div>
-  )
+	const Icon = Icons[name];
+	const copyName = () => {
+		copy(name);
+		toast(`${name} has been copied to clipboard.`, {
+			icon: "🎉",
+			style: {
+				borderRadius: "4px",
+				background: "#1e1e20",
+				color: "#fff",
+				padding: "4px 8px",
+				fontSize: 14,
+			},
+		});
+	};
+	return (
+		<div
+			onClick={copyName}
+			className="flex flex-col items-center justify-center col-span-1 p-8 transition-all transform rounded cursor-pointer bg-gray-50 hover:bg-gray-100"
+		>
+			<Icon size={24} color="black" />
+			<div className="mt-2 text-xs text-gray-700">{name}</div>
+		</div>
+	);
 }
