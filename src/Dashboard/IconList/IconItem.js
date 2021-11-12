732e846fbb3e2550d4d1df79ed04b9a4c50192d4
amaldinesh7 <amalkdinesh@gmail.com>
2021-11-12 14:51:29 +0530

    Icons display style updated

--- src/Dashboard/IconList/IconItem.js
+++ src/Dashboard/IconList/IconItem.js
@@ -4,27 +4,27 @@ import copy from "copy-to-clipboard";
 import toast from "react-hot-toast";
 
 export default function IconItem({ name }) {
-	const Icon = Icons[name];
-	const copyName = () => {
-		copy(name);
-		toast(`${name} has been copied to clipboard.`, {
-			icon: "🎉",
-			style: {
-				borderRadius: "4px",
-				background: "#1e1e20",
-				color: "#fff",
-				padding: "4px 8px",
-				fontSize: 14,
-			},
-		});
-	};
-	return (
-		<div
-			onClick={copyName}
-			className="flex flex-col items-center justify-center col-span-1 p-8 transition-all transform rounded cursor-pointer bg-gray-50 hover:bg-gray-100"
-		>
-			<Icon size={24} color="black" />
-			<div className="mt-2 text-xs text-gray-700">{name}</div>
-		</div>
-	);
+  const Icon = Icons[name];
+  const copyName = () => {
+    copy(name);
+    toast(`${name} has been copied to clipboard.`, {
+      icon: "🎉",
+      style: {
+        borderRadius: "4px",
+        background: "#1e1e20",
+        color: "#fff",
+        padding: "4px 8px",
+        fontSize: 14,
+      },
+    });
+  };
+  return (
+    <div
+      onClick={copyName}
+      className="flex flex-col items-center justify-center col-span-1 p-8 transition-all transform bg-white border rounded cursor-pointer hover:bg-gray-100"
+    >
+      <Icon size={24} color="#68737D" />
+      <div className="mt-2 text-xs text-gray-700">{name}</div>
+    </div>
+  );
 }
